2a12588f0f2e8690ac45137dd690dd638a89dd7a
magosa <nwtwh820@gmail.com>
2020-09-15 13:42:31 +0900

    Add files via upload

--- bin_sensor.py
+++ bin_sensor.py
@@ -9,45 +9,45 @@ import time
 TOPIC = "test/001"
 SENSOR = 18
 
-# ブローカーに接続できたときの処理
-def on_connect(client, userdata, flag, rc):
-  print("Connected with result code " + str(rc))
 
-# ブローカーが切断したときの処理
-def on_disconnect(client, userdata, flag, rc):
-  if rc != 0:
-     print("Unexpected disconnection.")
+def onConnect(client, userdata, flag, rc):  # ブローカーに接続できたときの処理
+    print("Connected with result code " + str(rc))
 
-# publishが完了したときの処理
-def on_publish(client, userdata, mid):
-  print("publish: {0}".format(mid))
 
-# メイン関数   この関数は末尾のif文から呼び出される
-def main():
-  client = mqtt.Client()                 # クラスのインスタンス(実体)の作成
-  client.on_connect = on_connect         # 接続時のコールバック関数を登録
-  client.on_disconnect = on_disconnect   # 切断時のコールバックを登録
-  client.on_publish = on_publish         # メッセージ送信時のコールバック
+def onDisconnect(client, userdata, flag, rc):  # ブローカーが切断したときの処理
+    if rc != 0:
+        print("Unexpected disconnection.")
 
-  client.connect("localhost", 1883, 60)  # 接続先は自分自身
 
-  pi = pigpio.pi()
-  pi.set_mode(SENSOR, pigpio.INPUT)
-  pi.set_pull_up_down(SENSOR, pigpio.PUD_DOWN)
+def onPublish(client, userdata, mid):  # publishが完了したときの処理
+    print("publish: {0}".format(mid))
 
-  # 通信処理スタート
-  client.loop_start()    # subはloop_forever()だが，pubはloop_start()で起動だけさせる
 
-  j_str = {}
-  # 永久に繰り返す
-  while True:
-    j_str['id'] = "infrared01"
-    j_str['time'] = time.time()
-    j_str['value'] = pi.read(SENSOR)
-    payload = json.dumps(j_str,ensure_ascii = False,indent=4)
-    print(payload)
-    client.publish(TOPIC,payload)    # トピック名とメッセージを決めて送信
-    time.sleep(5)   # 3秒待つ
+def main():  # メイン関数   この関数は末尾のif文から呼び出される
+    client = mqtt.Client()                 # クラスのインスタンス(実体)の作成
+    client.on_connect = onConnect         # 接続時のコールバック関数を登録
+    client.on_disconnect = onDisconnect   # 切断時のコールバックを登録
+    client.on_publish = onPublish         # メッセージ送信時のコールバック
+
+    client.connect("localhost", 1883, 60)  # 接続先は自分自身
+
+    pi = pigpio.pi()
+    pi.set_mode(SENSOR, pigpio.INPUT)
+    pi.set_pull_up_down(SENSOR, pigpio.PUD_DOWN)
+
+    # 通信処理スタート
+    client.loop_start()    # subはloop_forever()だが，pubはloop_start()で起動だけさせる
+
+    json_str = {}
+    # 永久に繰り返す
+    while True:
+        json_str['value'] = pi.read(SENSOR)
+        json_str['datetime'] = time.time()
+        json_str['publisher'] = "infrared01"
+        payload = json.dumps(json_str, ensure_ascii=False, indent=4)
+        client.publish(TOPIC, payload)    # トピック名とメッセージを決めて送信
+        time.sleep(5)   # 5秒待つ
+
 
 if __name__ == '__main__':          # importされないときだけmain()を呼ぶ
-  main()    # メイン関数を呼び出す
+    main()    # メイン関数を呼び出す
